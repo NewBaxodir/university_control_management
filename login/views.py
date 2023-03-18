53167779c5a40c416dc8eacfc91299042c8352f1
NewBaxodir <76239402+NewBaxodir@users.noreply.github.com>
2023-03-18 10:43:57 +0500

    Update views.py

--- login/views.py
+++ login/views.py
@@ -1,19 +1,28 @@
-from django.shortcuts import render
-from django.contrib.auth import login, logout,authenticate
+from django.shortcuts import render, redirect
+from django.contrib.auth import login as login_func, logout as logout_func,authenticate
+from django.contrib import messages
+from django.core.handlers.wsgi import WSGIRequest
+
 # Create your views here.
 
 
 
 
-def login(request):
+
+def login(request: WSGIRequest):
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
         user = authenticate(request, username=username, password=password)
         if user is not None:
-            login(request, user)
+            login_func(request, user)
             return redirect('home_uquv_management')
         else:
             messages.info(request, 'Login or password error')
     context = {}
     return render(request, 'login/login.html',context)
+
+
+def logout(request: WSGIRequest):
+    logout_func(request)
+    return redirect('auth:login')
