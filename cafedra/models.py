2afd046241f19abc99d69ed8b4c5ba24d66f08a8
NewBaxodir <76239402+NewBaxodir@users.noreply.github.com>
2023-03-19 22:35:31 +0500

    Update models.py

--- cafedra/models.py
+++ cafedra/models.py
@@ -1,12 +1,11 @@
 from django.db import models
 
-
-
 from moderator.models import MainUser
 from fakultet.models import Faculty
 
 class CafedraManager(models.Model):
     user = models.OneToOneField(MainUser, on_delete=models.CASCADE, verbose_name='Foydalanuvchi')
+    is_manager = models.BooleanField(default=False)
 
     def __str__(self) -> str:
         return str(self.user)
@@ -18,7 +17,7 @@ class CafedraManager(models.Model):
 
 
 class Cafedra(models.Model):
-    name = models.CharField(max_length=200, verbose_name='Kafedra nomi')
+    name = models.CharField(max_length=200, verbose_name='Kafedra nomi', unique=True)
     faculty = models.ForeignKey(Faculty, on_delete=models.CASCADE, verbose_name='Fakulteti')
     manager = models.ForeignKey(CafedraManager, on_delete=models.CASCADE, verbose_name='Kafedra manageri')
 
