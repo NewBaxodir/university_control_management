04a97affaf2673821163a28bc53d126cb2383834
NewBaxodir <76239402+NewBaxodir@users.noreply.github.com>
2023-03-19 17:12:16 +0500

    Update urls.py

--- educational_department/urls.py
+++ educational_department/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from educational_department import views 
 
 
+
+
+# namespace='educational_department'
 urlpatterns = [
     path('', views.home_uquv_management, name='home'),
     path('add/hr', views.add_hr_views, name='add_hr'),   
