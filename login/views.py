4906cb6c2812235c6180eeaf0a89ee9f052d14e9
NewBaxodir <76239402+NewBaxodir@users.noreply.github.com>
2023-03-19 14:56:17 +0500

    Update views.py

--- login/views.py
+++ login/views.py
@@ -3,10 +3,11 @@ from django.contrib.auth import login as login_func, logout as logout_func,authe
 from django.contrib import messages
 from django.core.handlers.wsgi import WSGIRequest
 
-# Create your views here.
+from django.contrib.auth.models import Group
 
-
-
+UQUV_MANAGEMENT = Group.objects.get(name='uquv_management')
+FACULTY_MANAGEMENT = Group.objects.get(name='faculty_management')
+KAFEDRA_MANAGEMENT = Group.objects.get(name='kafedra_management')
 
 
 def login(request: WSGIRequest):
@@ -15,8 +16,26 @@ def login(request: WSGIRequest):
         password = request.POST.get('password')
         user = authenticate(request, username=username, password=password)
         if user is not None:
-            login_func(request, user)
-            return redirect('educational_department:home')
+            login_func(request, user) 
+            # O'quv bo'limi odami ekanligini tekshirish
+            try:
+                user.groups.get(pk=UQUV_MANAGEMENT.pk)
+                return redirect('educational_department:home')
+            except: pass
+            
+            # Dekanat odami ekanligini tekshirish
+            try:
+                user.groups.get(pk=FACULTY_MANAGEMENT.pk)
+                return redirect('fakultet:faculty_dep_home')
+            except: pass
+            
+            # Kafedra mudiri ekanligini tekshirish
+            try:
+                user.groups.get(pk=KAFEDRA_MANAGEMENT.pk)
+                return redirect('cafedra:dep_home')
+            except: pass
+
+            logout_func(request)           
         else:
             messages.info(request, 'Login or password error')
     context = {}
