621bd7ca0b1cf63fcc3c97535b1d48f7fc6e7986
NewBaxodir <76239402+NewBaxodir@users.noreply.github.com>
2023-03-20 11:50:15 +0500

    Update urls.py

--- educational_department/urls.py
+++ educational_department/urls.py
@@ -10,5 +10,7 @@ urlpatterns = [
     path('add/hr', views.add_hr_views, name='add_hr'),   
     path('add/cafedra', views.add_cafedra_view, name='add_cafedra'),
     path('add/faculty', views.add_faculty_view, name='add_faculty'),
+    path('add/edu_manager', views.add_edu_dep_manager, name='edu_manager'),
+    path('list/cafedra', views.list_cafedra_view, name='list_cafedra'),
 ]
 
